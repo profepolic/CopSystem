c6b27e6b370115114ad6566d3e72dc17a84d9b60
profepolic <gabrielbig1778@gmail.com>
2022-12-27 20:41:23 -0300

    alteracao

--- 404.php
+++ 404.php
@@ -1,1 +1,3 @@
+sss
+
 <style>h1, h3, p{color: #fff;}body{margin: 0px;}.main-footer{background: #444;position: fixed;top: 72%;width: 100%;height: 18%;padding: 33px;box-shadow: 0 0 22px #000;color: #fff;}</style><center><h1 class='teste' style='color: #fff; font-size: 150px;'>404</h1><h3>Página não encontrada</h3><p>O recurso solicitado nao foi encontrado neste servidor!</p><center>
