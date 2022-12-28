97dfa11f879d423cfc02642219a02a80086b7578
Marckson <marcksonasantos@gmail.com>
2022-12-27 23:58:34 -0300

    correcao pagina suporte

--- suporte.php
+++ suporte.php
@@ -1,32 +1,6 @@
 <style>
 .chat{
     margin: auto;
-}
-</style>
-
-<div class="row">
-    <div class="col-lg-12">
-        <div class="col-lg-8 chat">
-            <div class="card-header">
-                    s
-            </div>
-            <div class="card-body">
-                    s
-            </div>
-            <div class="card-footer">
-                    s
-            </div>
-        </div>
-        
-            <!-- <div class="form-group">
-              <label for="exampleFormControlTextarea1">Example textarea</label>
-              <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-            </div> -->
-    </div>
-</div>
-<style>
-.chat{
-    margin: auto;
     background: #fff;
     padding: 20px;
 }
