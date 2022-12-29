6e4a25269da4f06b2e18e69077b0ed6a46435ca2
profepolic <gabrielbig1778@gmail.com>
2022-12-29 05:26:24 -0300

    atualiza

--- include/buscar_user.php
+++ include/buscar_user.php
@@ -31,14 +31,15 @@
 
 <div class="row col-12 col-lg-12">
 <div class="img-user col-2 col-lg-2">
+<div style="margin-right: 10px; height: 60px; width: 65px;  background: url(https://www.habbo.com.br/habbo-imaging/avatarimage?&user=<?php echo $user['nome'] ;?>&action=&direction=4&head_direction=3&img_format=png&gesture=&headonly=0&size=2),  radial-gradient(circle, #fff, #f1f1f1); background-position: center top -10px; border-radius: 60px; "></div>
     <img src="http://www.habbo.com.br/habbo-imaging/avatarimage?&user=<?php echo $user['nome']; ?>&action=std&direction=2&head_direction=2&img_format=png&gesture=std&headonly=0&size=b">
 </div>
 <div class="col-2 col-lg-2 mt-5">
 <?php
         if($user['situacao'] == 1){
-            echo "<span style='color: green'>ATIVO</span>";
+            echo "<span class='badge badge-pill badge-success'>ATIVO</span>";
         }else{
-            echo "<span style='color: red'>DESATIVADO</span>";
+            echo "<span class='badge badge-danger' >DESATIVADO</span>";
         }
     ?>
 </div>
