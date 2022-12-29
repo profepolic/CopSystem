8c5c66dbefb2e9231986593e0a85dbd49e6c4615
Marckson <marcksonasantos@gmail.com>
2022-12-29 05:59:39 -0300

    botao config modal

--- js/modal_inicio.js
+++ js/modal_inicio.js
@@ -89,3 +89,55 @@ $("#user").on('click', ()=>{
         }
     })
 })
+
+
+$("#config").on('click', ()=>{
+    var html = '<div class="modal fade" id="modal" tabindex="-1" role="dialog" aria-hidden="true">' + 
+    '<div class="modal-dialog modal-dialog-centered modal-dialog-scrollable" role="document">' + 
+               '<div class="modal-content">' +
+               '<div class="modal-header">' +
+               '<h5 id="titulo_modal" class="modal-title"></h5>' +
+               '<button id="fechar" type="button" class="close" aria-label="Fechar">' +
+               '<span aria-hidden="true" style="color:white">&times;</span>' +
+               '</button>' +
+               '</div>' +
+               '<div id="body_modal" class="modal-body">' +
+               '</div>' +
+               '</div>' +
+               '</div>' +
+           '</div>';
+    $(document.body).append(html);
+    $.ajax({
+        url: 'include/pesquisa_user.php',
+        method: 'GET',
+        cache: false,
+        contentType: false,
+        processData: false,
+        success: function(resultado){
+            $("#titulo_modal").html('Pesquisar Militar');
+            $("#body_modal").html(resultado);
+            $("#modal").modal({backdrop:'static', keyboard:false});
+    $("#modal").modal('show');
+    $(".modal-content").addClass('fundo-modal');
+    $("#fechar").on('click', ()=>{
+        Promise.all([$("#modal").modal('hide')]).then(function() {
+            $(".modal-backdrop").remove();
+            $("#modal").remove();
+          });
+    })
+
+    $("#botao_pesquisar").on('click', ()=>{
+        $.ajax({
+            url: 'include/buscar_user.php?nick='+$("#pesquisa").val(),
+            method: 'GET',
+            cache: false,
+            contentType: false,
+            processData: false,
+            success: function(resultado){
+                $("#resultado").html(resultado);
+            }
+        })
+    })
+        }
+    })
+})
