0341eec5a86e985126f08737897bc1dbdcffee2a
profepolic <gabrielbig1778@gmail.com>
2023-01-06 08:49:04 -0300

    atualiza geral

--- include/botaosuporte.php
+++ include/botaosuporte.php
@@ -1,3 +1,35 @@
+<style>
+.crud_body {
+    position: fixed;
+    top: 0;
+    width: 31rem;
+    display: none;
+    left: 54rem;
+    z-index: 99;
+}
+</style>
+
+
 <div class="menu-flot">
-  <a class="menu-float link" href="#"><i class="fas fa-comments"><p class="menu-float nome">Suporte</p></i></a>
+  <a class="menu-float link" href="#" onclick="suporte()"><i class="fas fa-comments"><p class="menu-float nome">Suporte</p></i></a>
 </div>
+
+<div class="crud_body">
+<iframe class="ifm" src="chat" frameborder="0"></iframe>
+</div>
+
+
+<script>
+
+  function suporte(){
+  
+  let crud_body = document.querySelector('.crud_body')
+
+  if(crud_body.style.display === "table"){
+    crud_body.style.display = 'none';
+  }else{
+    crud_body.style.display = 'table';
+  }
+
+}
+</script>
--- suporte.php
+++ suporte.php
@@ -1,9 +1,1 @@
-<style>
-.ifm {
-    display: table;
-    width: 100%;
-    height: 100vh;
-}
-</style>
-
 <iframe class="ifm" src="chat" frameborder="0"></iframe>
